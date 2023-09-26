3c8d7f1edb775ad0de76d2bd16338474e87be11b
Edward Rees <erees.hk@gmail.com>
2023-09-26 14:23:41 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple Fastify Todo App
+
+A really simple CRUD Todo App written in Fastify and Bun.
+
+## Technology
+
+- Fastify
+- Bun runtime (Can use NodeJS instead)
+- Supabase (PostgreSQL)
+
+## Setup
+
+1. Clone this repository
+2. Install dependencies
+3. Create `.env` file and fill it with your Supabase credentials as `SUPABASE_URL` and `SUPABASE_ANON_KEY`
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async (req: FastifyRequest, reply: FastifyReply) => {
+  reply.send({ hello: "world" });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 || process.env.PORT });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
